125f239d3e911c3e59d1373902a0f60c7f9b4998
wickstjo <33825194+wickstjo@users.noreply.github.com>
2018-10-20 20:28:51 +0300

    Added scaling windows

--- js/chart.js
+++ js/chart.js
@@ -8,17 +8,6 @@ window.onload = () => { drawImage(image); };
 // CHART RENDERING FUNC
 function drawImage(image) {
 
-   // CONSTRUCT IMAGE OBJECT
-   var image = {
-      obj: image,
-      width: image.width,
-      height: image.height,
-      margin: {
-         x: 0,
-         y: 0
-      }
-   }
-
    // SET IMAGE CANVAS
    $('body').append('<canvas id="myCanvas"></canvas>');
    var canvas = $('#myCanvas')[0];
@@ -27,53 +16,71 @@ function drawImage(image) {
 
    // DRAW IMAGE TO CANVAS
    var context = canvas.getContext('2d');
-   context.drawImage(image.obj, image.margin.x, image.margin.y);
+   context.drawImage(image, 0, 0);
    
    // HIDE GENERATED IMAGE
    $('#myCanvas').css('display', 'none');
 
+   // GENERATE SELECTORS
+   var selectors = `
+      <div id="shared">
+         <table><tr>
+            <td><div id="red"></div></td>
+            <td><div id="green"></div></td>
+            <td><div id="blue"></div></td>
+         </tr></table>
+      </div>
+      <div id="relational"></div>
+   `;
+
+   // RENDER THEM IN
+   $('body').append(selectors);
+
    // FETCH IMAGE DATA & CONSTRUCT DATA OBJECT
    var data = {
-      content: context.getImageData(image.margin.x, image.margin.y, image.width, image.height).data,
+      content: context.getImageData(0, 0, canvas.width, canvas.height).data,
       red: {
          values: [],
          highest: 0,
-         path: ''
+         paths: {
+            relational: '',
+            solo: ''
+         }
       },
       green: {
          values: [],
          highest: 0,
-         path: ''
+         paths: {
+            relational: '',
+            solo: ''
+         }
       },
       blue: {
          values: [],
          highest: 0,
-         path: ''
+         paths: {
+            relational: '',
+            solo: ''
+         }
       }
    }
 
-   // FILL RGB ARRAYS WITH 255 x 0
-   for (var z = 0; z < 256; z++) {
-      data.red.values.push(0);
-      data.green.values.push(0);
-      data.blue.values.push(0);
+   // CSS SETTINGS -- HELP OBJECT FOR SETTINGS
+   var css = {
+      border: { size: 4 },
+      padding: { body: 30, div: 15 }
    }
-
-   // INTERATE THROUGH EVERY PIXEL -- INCREMENT COLOR NUANCE OCCURANCE BY ONE
-   for(var i = 0; i < data.content.length; i+=4) {
-      data.red.values[data.content[i]] += 1;
-      data.green.values[data.content[i + 1]] += 1;
-      data.blue.values[data.content[i + 2]] += 1;
-   }
-
-   // CONSTRUCT DIV FOR GRAPH
-   var div = '<div id="box"></div>';
-   $('body').append(div);
 
    // SETTINGS OBJECT
    var settings = {
-      width: $('#box').width(),
-      height: $('#box').height(),
+      width: {
+         large: window.innerWidth - (css.border.size * 2) - (css.padding.body * 2),
+         small: (window.innerWidth - (css.border.size * 6) - (css.padding.body * 2) - (css.padding.div * 2)) / 3
+      },
+      height: {
+         large: (window.innerHeight - (css.padding.body * 2) - (css.border.size * 4) - (css.padding.div)) * 0.75,
+         small: (window.innerHeight - (css.padding.body * 2) - (css.border.size * 4) - (css.padding.div)) * 0.25, 
+      },
       background: {
          red: '#D86666',
          green: '#5ECA66',
@@ -81,13 +88,33 @@ function drawImage(image) {
       },
       border: {
          color: 'white',
-         size: 1
+         size: 0
       },
       opacity: 0.6,
       cluster: 15
    }
 
-   // MAKE CLUSTERS FOR READABILITY
+   // SET SCALED WIDTHS AND HEIGHTS OF SELECTORS
+   $('#red, #green, #blue').width(settings.width.small)
+   $('#red, #green, #blue').height(settings.height.small)
+   $('#relational').width(settings.width.large)
+   $('#relational').height(settings.height.large)
+
+   // FILL RGB ARRAYS WITH 255 x 0
+   for (var z = 0; z < 256; z++) {
+      data.red.values.push(0);
+      data.green.values.push(0);
+      data.blue.values.push(0);  
+   }
+
+   // INTERATE THROUGH EVERY PIXEL -- INCREMENT COLOR NUANCE INDEX
+   for(var i = 0; i < data.content.length; i+=4) {
+      data.red.values[data.content[i]] += 1;
+      data.green.values[data.content[i + 1]] += 1;
+      data.blue.values[data.content[i + 2]] += 1;
+   }
+
+   // MAKE COLOR CLUSTERS FOR READABILITY
    data.red.values = clusterify(data.red.values, settings.cluster);
    data.green.values = clusterify(data.green.values, settings.cluster);
    data.blue.values = clusterify(data.blue.values, settings.cluster);
@@ -97,57 +124,86 @@ function drawImage(image) {
    data.green.highest = d3.max(data.green.values);
    data.blue.highest = d3.max(data.blue.values);
 
-   // Y-SCALING -- BASED ON OVERALL HIGHEST VALUE
-   var yScale = d3.scaleLinear()
-      .domain([0, d3.max([data.red.highest, data.blue.highest, data.green.highest])])
-      .range([0, settings.height])
+   // GENERATE & PUSH RELATIONAL PATHS
+   data = relational_paths(data, settings);
 
-   // X-SCALING
-   var xScale = d3.scaleTime()
-      .domain([0, data.red.values.length - 1])
-      .rangeRound([0, settings.width])
+      // GENERATE GRAPH CANVAS
+      var canvas = d3.select('#relational').append('svg')
+         .attr('width', settings.width.large)
+         .attr('height', settings.height.large)
 
-   // GENERATE PATH METHOD
-   var pathify = d3.area()
-      .x((data, i) => { return xScale(i) })
-      .y0(settings.height - yScale(0))
-      .y1((data) => { return settings.height - yScale(data) })
+      // RED PATH
+      canvas.append('path')
+         .attr('fill', settings.background.red)
+         .attr('stroke', settings.border.color)
+         .attr('stroke-width', settings.border.size)
+         .attr('d', data.red.paths.relational)
+         .attr('opacity', settings.opacity)
 
+      // GREEN PATH
+      canvas.append('path')
+         .attr('fill', settings.background.green)
+         .attr('stroke', settings.border.color)
+         .attr('stroke-width', settings.border.size)
+         .attr('d', data.green.paths.relational)
+         .attr('opacity', settings.opacity)
 
-   // CONVERT XY OBJECTS INTO D3 PATHS
-   data.red.path = pathify(data.red.values);
-   data.green.path = pathify(data.green.values);
-   data.blue.path = pathify(data.blue.values);
+      // BLUE PATH
+      canvas.append('path')
+         .attr('fill', settings.background.blue)
+         .attr('stroke', settings.border.color)
+         .attr('stroke-width', settings.border.size)
+         .attr('d', data.blue.paths.relational)
+         .attr('opacity', settings.opacity)
+
+   // GENERATE & PUSH RED PATH
+   data.red.paths.solo = solo_path(data.red, settings);
+
+      // GENERATE GRAPH CANVAS
+      var canvas = d3.select('#red').append('svg')
+         .attr('width', settings.width.small)
+         .attr('height', settings.height.small)
+
+      // RED PATH
+      canvas.append('path')
+         .attr('fill', settings.background.red)
+         .attr('stroke', settings.border.color)
+         .attr('stroke-width', settings.border.size)
+         .attr('d', data.red.paths.solo)
+         .attr('opacity', settings.opacity)
+
+   // GENERATE & PUSH GREEN PATH
+   data.green.paths.solo = solo_path(data.green, settings);
+
+      // GENERATE GRAPH CANVAS
+      var canvas = d3.select('#green').append('svg')
+         .attr('width', settings.width.small)
+         .attr('height', settings.height.small)
+
+      // GREEN PATH
+      canvas.append('path')
+         .attr('fill', settings.background.green)
+         .attr('stroke', settings.border.color)
+         .attr('stroke-width', settings.border.size)
+         .attr('d', data.green.paths.solo)
+         .attr('opacity', settings.opacity)
+
+   // GENERATE & PUSH BLUE PATH
+   data.blue.paths.solo = solo_path(data.blue, settings);
+
+      // GENERATE GRAPH CANVAS
+      var canvas = d3.select('#blue').append('svg')
+         .attr('width', settings.width.small)
+         .attr('height', settings.height.small)
+
+      // BLUE PATH
+      canvas.append('path')
+         .attr('fill', settings.background.blue)
+         .attr('stroke', settings.border.color)
+         .attr('stroke-width', settings.border.size)
+         .attr('d', data.blue.paths.solo)
+         .attr('opacity', settings.opacity)
 
    log(data)
-
-   // GENERATE GRAPH CANVAS
-   var canvas = d3.select('#box').append('svg')
-      .attr('width', settings.width)
-      .attr('height', settings.height)
-
-   // RED PATH
-   canvas.append('path')
-      .attr('fill', settings.background.red)
-      .attr('stroke', settings.border.color)
-      .attr('stroke-width', settings.border.size)
-      .attr('d', data.red.path)
-      .attr('opacity', settings.opacity)
-
-   // GREEN PATH
-   canvas.append('path')
-      .attr('fill', settings.background.green)
-      .attr('stroke', settings.border.color)
-      .attr('stroke-width', settings.border.size)
-      .attr('d', data.green.path)
-      .attr('opacity', settings.opacity)
-
-   // BLUE PATH
-   canvas.append('path')
-      .attr('fill', settings.background.blue)
-      .attr('stroke', settings.border.color)
-      .attr('stroke-width', settings.border.size)
-      .attr('d', data.blue.path)
-      .attr('opacity', settings.opacity)
-
+   log(settings)
 }
--- js/misc.js
+++ js/misc.js
@@ -25,5 +25,56 @@ function clusterify(colors, size) {
    return cluster;
 }
 
+// GENERATE RELATIONAL PATHS FOR RGB CHART
+function relational_paths(data, settings) {
+
+   // Y-SCALING -- BASED ON OVERALL HIGHEST VALUE
+   var yScale = d3.scaleLinear()
+      .domain([0, d3.max([data.red.highest, data.blue.highest, data.green.highest])])
+      .range([0, settings.height.large])
+
+   // X-SCALING
+   var xScale = d3.scaleTime()
+      .domain([0, data.red.values.length - 1])
+      .rangeRound([0, settings.width.large])
+
+   // GENERATE PATH METHOD
+   var pathify = d3.area()
+      .x((data, i) => { return xScale(i) })
+      .y0(settings.height.large - yScale(0))
+      .y1((data) => { return settings.height.large - yScale(data) })
+
+   // CONVERT SCALED VALUES INTO PATHS
+   data.red.paths.relational = pathify(data.red.values);
+   data.green.paths.relational = pathify(data.green.values);
+   data.blue.paths.relational = pathify(data.blue.values);
+
+   // RETURN MODIFIED DATA
+   return data;
+}
+
+// GENERATE PATH FOR A SINGLE COLOR
+function solo_path(data, settings) {
+
+   // Y-SCALING
+   var yScale = d3.scaleLinear()
+      .domain([0, data.highest])
+      .range([0, settings.height.small])
+
+   // X-SCALING
+   var xScale = d3.scaleTime()
+      .domain([0, data.values.length - 1])
+      .rangeRound([0, settings.width.small])
+
+   // GENERATE PATH METHOD
+   var pathify = d3.area()
+      .x((data, i) => { return xScale(i) })
+      .y0(settings.height.small - yScale(0))
+      .y1((data) => { return settings.height.small - yScale(data) })
+
+   // GENERATE & RETURN PATH
+   return pathify(data.values);
+}
+
 // SHORTHAND FOR CONSOLE LOGGING
 function log(stuff) { console.log(stuff); }
